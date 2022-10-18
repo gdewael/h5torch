bfc3d850f342597578a7d6cfaab50a8b04fe6b98
gdewael <gaetandewaele@hotmail.com>
2022-10-18 07:34:45 +0000

    new setup and github actions

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-import sys
 from setuptools import setup, find_packages
-
-sys.path[0:0] = ["h5torch"]
-from version import __version__
 
 from pathlib import Path
 this_directory = Path(__file__).parent
@@ -12,7 +8,6 @@ setup(
     name="h5torch",
     python_requires=">3.9.0",
     packages=find_packages(),
-    version=__version__,
     license="LICENSE.txt",
     description="h5torch",
     long_description=long_description,
